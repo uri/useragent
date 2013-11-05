e7aa9a650137d8eae2d5c757c732cd2bda30dae4
ugorelik <uri.gore@gmail.com>
2013-11-05 17:16:39 -0500

    Cleaned up some redundant code.

--- lib/user_agent/browsers/internet_explorer_11.rb
+++ lib/user_agent/browsers/internet_explorer_11.rb
@@ -1,6 +1,6 @@
 class UserAgent
   module Browsers
-    class InternetExplorer11 < Base
+    class InternetExplorer11 < UserAgent::Browsers::InternetExplorer
       def self.extend?(agent)
         agent.application &&
           agent.application.comment &&
@@ -18,28 +18,6 @@ class UserAgent
 
       def compatibility
         "compatible"
-      end
-
-      def compatible?
-        compatibility == "compatible"
-      end
-
-      def compatibility_view?
-        version == "7.0" && application.comment.detect { |c| c['Trident/'] }
-      end
-
-      # Before version 4.0, Chrome Frame declared itself (unversioned) in a comment;
-      # as of 4.0 it can declare itself versioned in a comment
-      # or as a separate product with a version
-      def chromeframe
-        cf = application.comment.include?("chromeframe") || detect_product("chromeframe")
-        return cf if cf
-        cf_comment = application.comment.detect { |c| c['chromeframe/'] }
-        cf_comment ? UserAgent.new(*cf_comment.split('/', 2)) : nil
-      end
-
-      def platform
-        "Windows"
       end
 
       def os
